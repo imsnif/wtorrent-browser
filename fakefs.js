b720a17a2159706def5c44845c3dddac2a801c21
Aram Drevekenin <grimsniffer@gmail.com>
2015-12-28 17:28:52 +0200

    Incorporated gist

---
+++ fakefs.js
@@ -0,0 +1,18 @@
+var files = {
+  '/etc/resolv.conf': 'nameserver 8.8.8.8\nnameserver 8.8.4.4\n',
+  '/etc/hosts': '127.0.0.1 localhost\n'
+}
+
+exports.readFile = function(path, options, callback) {
+  if (typeof options === 'function') {
+    callback = options
+    options = null
+  }
+  if (typeof files[path] === 'string') {
+    setTimeout(callback.bind(null, null, files[path]), 0);
+  } else {
+    var e = new Error('ENOENT: '+path)
+    e.code = 'ENOENT'
+    setTimeout(callback.bind(null, e), 0);
+  }
+}
